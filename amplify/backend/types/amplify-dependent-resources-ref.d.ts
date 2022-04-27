3c82fcaa3c832f96029f20b877fb0f3ee5e5c89c
Aishwarya Velumani <62029435+AishwaryaVelumani@users.noreply.github.com>
2022-04-27 15:17:08 +0530

    removed cover folder and added all the files correctly

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp23e1b4e5a": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "amplifyapp2": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "s3dd911b9c": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
